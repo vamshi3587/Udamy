817f9ee4d9c4626be53c4b81fcac245ab8d5af4e
vamshi3587 <61615694+vamshi3587@users.noreply.github.com>
2022-01-20 17:00:18 +0530

    configuration completed

--- Cards/src/main/java/com/vbank/cards/CardsApplication.java
+++ Cards/src/main/java/com/vbank/cards/CardsApplication.java
@@ -3,12 +3,13 @@ package com.vbank.cards;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScans;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-
+@RefreshScope
 @ComponentScans({ @ComponentScan("com.vbank.cards.controller") })
 @EnableJpaRepositories("com.vbank.cards.repository")
 @EntityScan("com.vbank.cards.model")
